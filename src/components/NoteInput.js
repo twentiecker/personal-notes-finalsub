6253a84750ecdb44b1db56039a390a30f61f1307
twentiecker <twentiecker@gmail.com>
2022-06-22 16:25:13 +0700

    add method onChange

--- src/components/NoteInput.js
+++ src/components/NoteInput.js
@@ -1,28 +1,65 @@
 import React from "react";
 import NoteInputTitleCharLimit from "./NoteInputTitleCharLimit";
 
-function NoteInput() {
-  return (
-    <div className="note-input">
-      <h2>Buat Catatan</h2>
-      <form>
-        <NoteInputTitleCharLimit />
-        <input
-          className="note-input__title"
-          type="text"
-          placeholder="Ini adalah judul ..."
-          required
-        />
-        <textarea
-          className="note-input__body"
-          type="text"
-          placeholder="Tuliskan catatanmu di sini ..."
-          required
-        />
-        <button type="submit">Buat</button>
-      </form>
-    </div>
-  );
+class NoteInput extends React.Component {
+  constructor(props) {
+    super(props);
+
+    // init state
+    this.state = {
+      title: "",
+      body: "",
+    };
+
+    this.onTitleChangeEventHandler = this.onTitleChangeEventHandler.bind(this);
+    this.onBodyChangeEventHandler = this.onBodyChangeEventHandler.bind(this);
+  }
+
+  onTitleChangeEventHandler(event) {
+    this.setState((prevState) => {
+      return {
+        ...prevState,
+        title: event.target.value,
+      };
+    });
+  }
+
+  onBodyChangeEventHandler(event) {
+    this.setState((prevState) => {
+      return {
+        ...prevState,
+        body: event.target.value,
+      };
+    });
+  }
+
+  render() {
+    return (
+      <div className="note-input">
+        <h2>Buat Catatan</h2>
+        <form>
+          <NoteInputTitleCharLimit />
+          <input
+            className="note-input__title"
+            type="text"
+            placeholder="Ini adalah judul ..."
+            value={this.state.title}
+            onChange={this.onTitleChangeEventHandler}
+            required
+          />
+          <textarea
+            className="note-input__body"
+            type="text"
+            placeholder="Tuliskan catatanmu di sini ..."
+            value={this.state.body}
+            onChange={this.onBodyChangeEventHandler}
+            required
+          />
+          <button type="submit">Buat</button>
+        </form>
+      </div>
+    );
+  }
 }
 
 export default NoteInput;
