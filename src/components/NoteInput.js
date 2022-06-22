1bd9d6422e02005699b2e21d6b5a6a19f75d1bdc
twentiecker <twentiecker@gmail.com>
2022-06-22 16:09:16 +0700

    add NoteInputTitleCharLimit

--- src/components/NoteInput.js
+++ src/components/NoteInput.js
@@ -1,9 +1,26 @@
 import React from "react";
+import NoteInputTitleCharLimit from "./NoteInputTitleCharLimit";
 
 function NoteInput() {
   return (
     <div className="note-input">
       <h2>Buat Catatan</h2>
+      <form>
+        <NoteInputTitleCharLimit />
+        <input
+          className="note-input__title"
+          type="text"
+          placeholder="Ini adalah judul ..."
+          required
+        />
+        <textarea
+          className="note-input__body"
+          type="text"
+          placeholder="Tuliskan catatanmu di sini ..."
+          required
+        />
+        <button type="submit">Buat</button>
+      </form>
     </div>
   );
 }
---
+++ src/components/NoteInputTitleCharLimit.js
@@ -0,0 +1,7 @@
+import React from "react";
+
+function NoteInputTitleCharLimit() {
+  return <p className="note-input__title__char-limit">Sisa Karakter:</p>;
+}
+
+export default NoteInputTitleCharLimit;
