3f05ff05a751af8c67fa5467bd33b095f504ae70
twentiecker <twentiecker@gmail.com>
2022-06-25 21:54:29 +0700

    import showFormattedDate and clean code

--- src/components/NoteItemContent.js
+++ src/components/NoteItemContent.js
@@ -1,17 +1,11 @@
 import React from "react";
+import { showFormattedDate } from "../utils/index";
 
 function NoteItemContent({ title, createdAt, body }) {
-  const dateTime = new Date(createdAt).toLocaleDateString("id-id", {
-    weekday: "long",
-    year: "numeric",
-    month: "long",
-    day: "numeric",
-  });
-
   return (
     <div className="note-item__content">
       <h3 className="note-item__title">{title}</h3>
-      <p className="note-item__date">{dateTime}</p>
+      <p className="note-item__date">{showFormattedDate(createdAt)}</p>
       <p className="note-item__body">{body}</p>
     </div>
   );
